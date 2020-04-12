97b6528b618a39361ff16849d2a23a6e777aed5d
masterful-array-3850 <crgammage@gmail.com>
2020-04-12 17:18:54 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,7 @@
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script src="js/selectors.js"></script>
+$(document).ready(function() {
+});
 function paragraphSelector() {
   document.getElementsByTagName('p')
 }
