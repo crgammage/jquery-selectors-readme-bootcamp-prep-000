c5a6ec5e5f4ec20d58949e2b7306a651086e32f6
masterful-array-3850 <crgammage@gmail.com>
2020-04-12 17:20:04 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,19 +1,19 @@
 <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
 <script src="js/selectors.js"></script>
 $(document).ready(function() {
+  function paragraphSelector() {
+    document.getElementsByTagName('p')
+  }
+  function lastImageSelector() {
+    $('img:last')
+  }
+  function ninjaBabySelector() {
+    $('#baby-ninja')
+  }
+  function divSelector() {
+    $(.pics)
+  }
+  function firstListItem() {
+    $('#pic-list ul:first-child')
+  }
 });
-function paragraphSelector() {
-  document.getElementsByTagName('p')
-}
-function lastImageSelector() {
-  $('img:last')
-}
-function ninjaBabySelector() {
-  $('#baby-ninja')
-}
-function divSelector() {
-  $(.pics)
-}
-function firstListItem() {
-  $('#pic-list ul:first-child')
-}
